af20fcde0f4243c233c33c74ca33afb660d36805
Marlone Akidiva <marlone.akidiva@clearlabs.com>
2024-03-18 02:41:15 +0300

    add search tasks and user management

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,36 +1,70 @@
 import { Router } from "express";
+import passport from "../../strategy/local-strategy.js";
+import { hashPassword } from "../../utils/helpers.js";
+import { usersList } from "../../utils/constants.js";
 
 const router = Router();
 
 router.post("/api/auth/register", (request, response) => {
-  const { body } = request;
-
-  const newUser = {
-    id: crypto.randomUUID(),
-    ...body,
-  };
-
-  mockUsers.users.push(newUser);
-
-  response.send(newUser);
-});
-
-router.post("/api/auth/login", (request, response) => {
   const {
     body: { email, password },
   } = request;
 
-  const findUser = mockUsers.users.find((user) => user.email === email);
-  if (!findUser || findUser.password !== password) {
-    return response.status(401).send({ msg: "BAD CREDENTIALS" });
-  }
+  const safePassword = hashPassword(password);
 
-  return response.status(200).send(findUser);
+  const newUser = {
+    id: crypto.randomUUID(),
+    email,
+    password: safePassword,
+  };
+
+  usersList.push(newUser);
+  response.sendStatus(200);
 });
 
-router.post("/api/auth/reset-password", (request, response) => {});
+router.post(
+  "/api/auth/login",
+  passport.authenticate("local"),
+  (request, response) => {
+    response.sendStatus(200);
+  }
+);
 
-router.post("/api/auth/logout", (request, response) => {});
-router.get("/api/auth/status", (request, response) => {});
+router.get("/api/auth/status", (request, response) => {
+  return request.user
+    ? response.status(200).send({
+        status: "authenticated",
+        userId: request.user,
+      })
+    : response.status(401).send({ message: "Not Authenticated" });
+});
+
+router.post("/api/auth/logout", (request, response) => {
+  console.log("request: ", request.isAuthenticated());
+
+  if (!request.user) {
+    return response.status(401).send({
+      message: "user not logged in",
+    });
+  }
+
+  if (request.isAuthenticated()) {
+    request.logOut((err) => {
+      if (err) {
+        return response.status(404).send({
+          error: err,
+        });
+      }
+    });
+
+    response.status(200).send({
+      message: "loggod out",
+    });
+  }
+
+  return response.status(401).send({
+    message: "Not Authenticated",
+  });
+});
 
 export default router;
