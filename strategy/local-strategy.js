92ab24ce38dc7b9316deb533822dd8b8c144878e
Marlone Akidiva <marlone.akidiva@clearlabs.com>
2024-03-17 06:45:15 +0300

    manage sessions using passport js

---
+++ strategy/local-strategy.js
@@ -0,0 +1,38 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+
+import { usersList } from "../constants.js";
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  try {
+    const findUser = usersList.find((user) => user.id === id);
+
+    if (!findUser) throw new Error("user not found");
+    done(null, findUser.id);
+  } catch (error) {
+    done(err, null);
+  }
+});
+
+passport.use(
+  new Strategy({ usernameField: "email" }, (email, password, done) => {
+    try {
+      const finduser = usersList.find((user) => user.email === email);
+
+      if (!finduser) throw new Error("user not found");
+
+      if (finduser.password !== password) {
+        throw new Error("invalid credentials");
+      }
+      done(null, finduser);
+    } catch (error) {
+      done(error, null);
+    }
+  })
+);
+
+export default passport;
